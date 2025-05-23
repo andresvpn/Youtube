8ad25fd12baafeb139bff4057f90c3125f21775d
andresvpn <125711121+andresvpn@users.noreply.github.com>
2025-05-23 06:06:24 -0500

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const config = require("../src/config")
 
 router.get('/', (req, res) => {
   const htmlPath = path.join(__dirname, '../public/index.html');
@@ -9,9 +10,9 @@ router.get('/', (req, res) => {
   
   // SEO básico para la página principal
   const seo = {
-    title: "PlayTube - Reproductor de Videos",
-    description: "Disfruta de los mejores videos en PlayTube. Busca, descubre y reproduce contenido de calidad.",
-    image: "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
+    title: config.title + "- Reproductor de Videos",
+    description: "Disfruta de los mejores videos en " + config.title + ". Busca, descubre y reproduce contenido de calidad.",
+    image: config.logo,
     url: req.protocol + '://' + req.get('host') + req.originalUrl
   };
   
@@ -27,7 +28,7 @@ router.get('/', (req, res) => {
     <meta name="twitter:card" content="summary_large_image">
   `;
   
-  html = html.replace('<title>PlayTube - Reproductor de Videos</title>', head);
+  html = html.replace('<title></title>', head);
   res.send(html);
 });
 
