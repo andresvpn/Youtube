349c2392d427f61d77e53140c5594a593c95358e
andresvpn <125711121+andresvpn@users.noreply.github.com>
2025-05-22 20:45:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configuración básica
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.use('/', require('./routes/home'));
+app.use('/watch', require('./routes/watch'));
+app.use('/api', require('./routes/api'));
+app.use('/search', require('./routes/search'));
+
+// Manejo de errores
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
+
+module.exports = app;
