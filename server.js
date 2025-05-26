3cba0345537f9d128b5f76c236ba626be3e1ae27
andresvpn <125711121+andresvpn@users.noreply.github.com>
2025-05-25 20:36:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 // Rutas
 app.use('/', require('./routes/home'));
 app.use('/watch', require('./routes/watch'));
-app.use('/api', require('./routes/api'));
 app.use('/search', require('./routes/search'));
+app.use('/api', (req, res, next) => {
+  const allowedOrigin = 'https://micTube.vercel.app';
+  const origin = req.get('origin') || req.get('referer');
+
+  if (origin && origin.startsWith(allowedOrigin)) {
+    next(); // Permitir acceso
+  } else {
+    res.status(403).json({ error: 'Acceso no autorizado' });
+  }
+}, require('./routes/api'));
 
 // Manejo de errores
 app.use((req, res) => {
